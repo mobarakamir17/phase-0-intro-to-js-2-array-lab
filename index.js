aa3a4f761309f16f3bd003733b9b253baad01290
Amir Mobarak <mobarakamir17@gmail.com>
2023-05-15 00:39:10 -0400

    array lab test

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Write your solution here!
+// test 1
+const cats = ["Milo", "Otis", "Garfield"];
+
+//test two
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+//test three
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  //test four
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+  //test five
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+  //test six
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+//test seven
+function prependCat(name) {
+  return [name, ...cats];
+}
+//test eight
+function removeLastCat() {
+    const newCatsArray = cats.slice(0, -1);
+    return newCatsArray;
+}
+//test nine
+function removeFirstCat() {
+    return cats.slice(1);
+  }
